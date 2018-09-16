ce95141000784f22cc0c8cde621d317181e957e2
Chris Woodle <chris@droplit.io>
2018-09-15 20:47:38 -0400

    cors?

--- anti-human-trafficking/src/server.ts
+++ anti-human-trafficking/src/server.ts
@@ -19,6 +19,12 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 interface ReportBody {
     victim: Person[],
     observedDate: string,
